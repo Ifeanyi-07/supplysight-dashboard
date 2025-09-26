3b2c786fbe4f38bf445ea711e9980a8419219690
ifeanyi <abahobiajulu@gmail.com>
2025-09-26 23:13:22 +0100

    frontend changes

--- supply-dashboard/src/main.jsx
+++ supply-dashboard/src/main.jsx
@@ -9,7 +9,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/c
 const client = new ApolloClient({
   link: new HttpLink({
     // uri: 'http://localhost:4000/graphql',  // ✅ backend GraphQL server
-    uri: import.meta.env.VITE_API_URL || "https://api.render.com/deploy/srv-d3bfp363jp1c73b0i9lg?key=huiGS48EMn4",
+    uri: "https://supplysight-dashboard.onrender.com/",
+
   }),
   cache: new InMemoryCache(),
 });
